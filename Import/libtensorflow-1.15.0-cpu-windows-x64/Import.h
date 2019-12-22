210750f7f874d4cc1dac265395633ee7d9f1f8ca
wxinix <xinwuping007@gmail.com>
2019-12-21 20:17:48 -0500

    Added sample projects, TensorFlow C-API import project, and updated README and LICENSE.

---
+++ Import/libtensorflow-1.15.0-cpu-windows-x64/Import.h
@@ -0,0 +1,18 @@
+﻿#import "tensorflow/c/c_api.h"
+#import "tensorflow/c/tf_attrtype.h"
+#import "tensorflow/c/tf_datatype.h"
+#import "tensorflow/c/tf_status.h"
+#import "tensorflow/c/tf_tensor.h"
+
+//
+// Tasks
+//
+// in Settings, pick the appropriate Mode (Toffee or Island), SubMode and SDK for the
+// import. Also set the ImportSearchPath to contain the folder or folders that congain
+// your header files
+//
+// In this file, add #import clauses for each header you want to import
+//
+// Finally, drag any binaries that need to be linked to use the imported library into
+// the project and make sure their type is set to "ImportLinkLibrary".
+//
---
+++ README.md
@@ -0,0 +1,3 @@
+# TensorFlow-Island
+RemObjects Island platform bindings for TensorFlow C API v1.15.0, and multiple RemObject Oxygene samples. The Island bindings allow using TensorFlow C API in several modern languages: Swift, Oxygene, Java and C#.
+
