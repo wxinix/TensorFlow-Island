933c7280e2c9329417ff0e821fecb9cccbdc0a79
wxinix <xinwuping007@gmail.com>
2020-01-18 19:19:10 -0500

    Code reformatting.

--- src/Classes/Helpers.pas
+++ src/Classes/Helpers.pas
@@ -27,50 +27,50 @@ uses
 type
   TensorFlowDataType nested in Helper = private enum 
   (
-    Float = TF_DataType.FLOAT, 
-    Double = TF_DataType.DOUBLE, 
-    Int32 = TF_DataType.INT32,
-    UInt8 = TF_DataType.UINT8,
-    Int16 = TF_DataType.INT16, 
-    Int8 = TF_DataType.INT8, 
-    String = TF_DataType.STRING, 
-    Complex64 = TF_DataType.COMPLEX64, 
-    Int64 = TF_DataType.INT64,
-    Bool = TF_DataType.BOOL , 
-    QInt8 = TF_DataType.QINT8, 
-    QUInt8 = TF_DataType.QUINT8,  
-    QInt32 = TF_DataType.QINT32,
-    BFloat16 = TF_DataType.BFLOAT16, 
-    QInt16 = TF_DataType.QINT16,
-    QUInt16 = TF_DataType.QUINT16,
-    UInt16 = TF_DataType.UINT16,
-    Complex128 = TF_DataType.COMPLEX128, 
-    Half = TF_DataType.HALF,
-    Resource = TF_DataType.RESOURCE,
-    Variant = TF_DataType.VARIANT,
-    UInt32 = TF_DataType.UINT32, 
-    UInt64 = TF_DataType.UINT64
+    Float              = TF_DataType.FLOAT, 
+    Double             = TF_DataType.DOUBLE, 
+    Int32              = TF_DataType.INT32,
+    UInt8              = TF_DataType.UINT8,
+    Int16              = TF_DataType.INT16, 
+    Int8               = TF_DataType.INT8, 
+    String             = TF_DataType.STRING, 
+    Complex64          = TF_DataType.COMPLEX64, 
+    Int64              = TF_DataType.INT64,
+    Bool               = TF_DataType.BOOL , 
+    QInt8              = TF_DataType.QINT8, 
+    QUInt8             = TF_DataType.QUINT8,  
+    QInt32             = TF_DataType.QINT32,
+    BFloat16           = TF_DataType.BFLOAT16, 
+    QInt16             = TF_DataType.QINT16,
+    QUInt16            = TF_DataType.QUINT16,
+    UInt16             = TF_DataType.UINT16,
+    Complex128         = TF_DataType.COMPLEX128, 
+    Half               = TF_DataType.HALF,
+    Resource           = TF_DataType.RESOURCE,
+    Variant            = TF_DataType.VARIANT,
+    UInt32             = TF_DataType.UINT32, 
+    UInt64             = TF_DataType.UINT64
   );
 
   TensorFlowCode nested in Helper = private enum 
   (
-    Ok = TF_Code.TF_OK,
-    Cancelled = TF_Code.TF_CANCELLED,
-    Unknown = TF_Code.TF_UNKNOWN,
-    InvalidArgument = TF_Code.TF_INVALID_ARGUMENT,
-    DeadlineExceed = TF_Code.TF_DEADLINE_EXCEEDED, 
-    NotFound = TF_Code.TF_NOT_FOUND,
-    AlreadyExists = TF_Code.TF_ALREADY_EXISTS,
-    PermissionDenied = TF_Code.TF_PERMISSION_DENIED,   
-    ResourceExhausted = TF_Code.TF_RESOURCE_EXHAUSTED,  
+    Ok                 = TF_Code.TF_OK,
+    Cancelled          = TF_Code.TF_CANCELLED,
+    Unknown            = TF_Code.TF_UNKNOWN,
+    InvalidArgument    = TF_Code.TF_INVALID_ARGUMENT,
+    DeadlineExceed     = TF_Code.TF_DEADLINE_EXCEEDED, 
+    NotFound           = TF_Code.TF_NOT_FOUND,
+    AlreadyExists      = TF_Code.TF_ALREADY_EXISTS,
+    PermissionDenied   = TF_Code.TF_PERMISSION_DENIED,   
+    ResourceExhausted  = TF_Code.TF_RESOURCE_EXHAUSTED,  
     FailedPrecondition = TF_Code.TF_FAILED_PRECONDITION, 
-    Aborted = TF_Code.TF_ABORTED, 
-    OutOfRange = TF_Code.TF_OUT_OF_RANGE,   
-    Unimplemented = TF_Code.TF_UNIMPLEMENTED, 
-    Internal = TF_Code.TF_INTERNAL, 
-    Unavailable = TF_Code.TF_UNAVAILABLE,  
-    DataLoss = TF_Code.TF_DATA_LOSS, 
-    Unauthenticated = TF_Code.TF_UNAUTHENTICATED
+    Aborted            = TF_Code.TF_ABORTED, 
+    OutOfRange         = TF_Code.TF_OUT_OF_RANGE,   
+    Unimplemented      = TF_Code.TF_UNIMPLEMENTED, 
+    Internal           = TF_Code.TF_INTERNAL, 
+    Unavailable        = TF_Code.TF_UNAVAILABLE,  
+    DataLoss           = TF_Code.TF_DATA_LOSS, 
+    Unauthenticated    = TF_Code.TF_UNAUTHENTICATED
   );
 
   LocalToTFDataTypeConvertException = public class(Exception)
@@ -107,30 +107,18 @@ type
     class method ConvertLocalTypeToTFDataType(aLocalType: &Type): TF_DataType;
     begin
       case aLocalType.Code of
-        TypeCodes.Boolean: 
-          result := TF_DataType.TF_BOOL;
-        TypeCodes.Byte: 
-          result := TF_DataType.TF_UINT8;
-        TypeCodes.UInt16: 
-          result := TF_DataType.TF_UINT16;
-        TypeCodes.UInt32: 
-          result := TF_DataType.TF_UINT32;
-        TypeCodes.UInt64: 
-          result := TF_DataType.TF_UINT64;
-        TypeCodes.SByte: 
-          result := TF_DataType.TF_INT8;
-        TypeCodes.Int16: 
-          result := TF_DataType.TF_INT16;
-        TypeCodes.Int32: 
-          result := TF_DataType.TF_INT32;
-        TypeCodes.Int64: 
-          result := TF_DataType.TF_INT64; 
-        TypeCodes.Single: 
-          result := TF_DataType.TF_FLOAT;
-        TypeCodes.Double: 
-          result := TF_DataType.TF_DOUBLE; 
-        TypeCodes.String: 
-          result := TF_DataType.TF_STRING;
+        TypeCodes.Boolean: result := TF_DataType.TF_BOOL;
+        TypeCodes.Byte   : result := TF_DataType.TF_UINT8;
+        TypeCodes.UInt16 : result := TF_DataType.TF_UINT16;
+        TypeCodes.UInt32 : result := TF_DataType.TF_UINT32;
+        TypeCodes.UInt64 : result := TF_DataType.TF_UINT64;
+        TypeCodes.SByte  : result := TF_DataType.TF_INT8;
+        TypeCodes.Int16  : result := TF_DataType.TF_INT16;
+        TypeCodes.Int32  : result := TF_DataType.TF_INT32;
+        TypeCodes.Int64  : result := TF_DataType.TF_INT64; 
+        TypeCodes.Single : result := TF_DataType.TF_FLOAT;
+        TypeCodes.Double : result := TF_DataType.TF_DOUBLE; 
+        TypeCodes.String : result := TF_DataType.TF_STRING;
       else
         raise new LocalToTFDataTypeConvertException(aLocalType);
       end;
