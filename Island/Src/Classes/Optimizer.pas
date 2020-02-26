4c15ae9529cc0e2ff27ad82aa0b5ad2bed60eb37
wxinix <xinwuping007@gmail.com>
2020-02-25 23:28:03 -0500

    Added Optimizer base class.

--- Island/Src/Classes/Optimizer.pas
+++ Island/Src/Classes/Optimizer.pas
@@ -27,7 +27,99 @@ uses
 
 type
   Optimizer = public abstract class
-  
+  private
+    fGraph: Graph;
+    fInitialAccumulatorValue: Single;
+    fIterations: Variable;
+    fLearningRate: Output;
+    fOptimizerName: String;
+    fUpdateOps: OperationList := new OperationList; readonly;
+  protected
+    method CreateDecayOps(aDecay: Single; aInitialLearningRate: NotNull<Output>): Output;
+    begin
+      if (aDecay <= 0) then begin
+        result := aInitialLearningRate;
+      end else begin
+        var decay := fGraph.Const(aDecay, 'Decay');
+        var one := fGraph.Const(Single(1));
+        result := fGraph.Mul(
+          aInitialLearningRate,
+          fGraph.Div(one,
+                     fGraph.Add(one,
+                                fGraph.Mul(decay,
+                                           fGraph.Cast(fGraph.Sub(fIterations.ReadAfter(fGraph.CurrentDependencies.ToArray),
+                                                                  fGraph.Const(Int64(1))),
+                                                       decay.OutputType)
+                                          )
+                              )
+                    ),
+          'learningrate');
+      end;
+    end;
+
+    method InitMoments(aGradientsAndVariables: not nullable array of Tuple of (Gradient: Output, Variable: Variable)): OutputList;
+    begin
+      result := new OutputList withCapacity(aGradientsAndVariables.Length);
+      for gv in aGradientsAndVariables do begin
+        var varType := gv.Variable.ReadHandle.OutputType;
+        var (nil, varShape) := fGraph.GetTensorShape(gv.Variable.ReadHandle);
+        var v := fGraph.VariableV2(varShape, varType);
+        result.Add(v);
+        fGraph.AddInitVariable(fGraph.Assign(v, fGraph.Constant(fInitialAccumulatorValue, varShape, varType)).Oper);
+      end;
+    end;
+  public
+    constructor(aGraph: NotNull<Graph>; const aOpName: NotNull<String>; aLearningRate, aDecay, aInitialAccumulatorValue: Single);
+    begin
+      if (aInitialAccumulatorValue < 0) then begin
+        raise new ArgumentException($'InitialAccumulatorValue = {aInitialAccumulatorValue}. It must be non-negative.');
+      end;
+
+      fGraph := aGraph;
+      fInitialAccumulatorValue := aInitialAccumulatorValue;
+      fOptimizerName := aOpName;
+
+      using newScope := fGraph.WithScope(fOptimizerName) do begin
+        fIterations := fGraph.MakeVariable(fGraph.Const(Int64(0)), false, 'iterations');
+        var initialLearningRate := fGraph.Const(aLearningRate);
+        var incOp := fGraph.AssignAddVariableOp(NotNull<Variable>(fIterations), fGraph.Const(Int64(1)));
+        fUpdateOps.Add(incOp);
+
+        using fGraph.WithDependencies([incOp]) do begin
+          fLearningRate := CreateDecayOps(aDecay, initialLearningRate);
+        end;
+      end;
+    end;
+
+    method ApplyGradient(aGradientsAndVariables: not nullable array of Tuple of (Gradient: Output, Variable: Variable)): OperationList; abstract;
+
+    method ComputeGradient(aLoss: NotNull<Output>; aVariables: array of Variable := nil; aColocateGradientsWithOps: Boolean := false): array of Tuple of (Gradient: Output, Variable: Variable); virtual;
+    begin
+      aVariables := if assigned(aVariables) then aVariables else fGraph.TrainableVariables;
+      result := new (Tuple of (Gradient: Output, Variable: Variable))[aVariables.Length];
+
+      for I: Integer := 0 to aVariables.Length - 1 do begin
+        var grad := fGraph.AddGradients([aLoss], [aVariables[I].ReadHandle]).Item2.First;
+        var var_ := aVariables[I];
+        result[I] := (grad, var_);
+
+        if aColocateGradientsWithOps then begin
+          var desc_ := new OperationDescription withGraph(fGraph) OpType(grad.Oper.OpType) OpName(grad.Oper.Name);
+          desc_.ColocateWith(var_.Resource.Oper);
+        end;
+      end;
+    end;
+
+    method Minimize(aLoss: NotNull<Output>; aVariables: array of Variable := nil): OperationList; virtual;
+    begin
+      var gv := ComputeGradient(aLoss, aVariables);
+      result := ApplyGradient(gv);
+    end;
+
+    property &Graph: Graph read fGraph; protected;
+    property OptimizerName: String read fOptimizerName; protected;
+    property Iterations: Variable read fIterations;
+    property LearningRate: Output read fLearningRate;
   end;
 
 end.
