a08bb056defd694ae8794d0273355ab06074ddc9
wxinix <xinwuping007@gmail.com>
2020-08-11 15:01:10 -0400

    Update ProtoGen.bat for TensorFlow 2.3.0

--- Island/Projects/OpGenerator/ProtoGen.bat
+++ Island/Projects/OpGenerator/ProtoGen.bat
@@ -1,1 +1,2 @@
-protogen.exe --proto_path=.\ProtoBufs\r1.15.0\ --csharp_out=.\ProtoGenFiles\r1.15.0\  *.proto
+REM protogen.exe --proto_path=.\ProtoBufs\r1.15.0\ --csharp_out=.\ProtoGenFiles\r1.15.0\  *.proto
+protogen.exe --proto_path=.\ProtoBufs\r2.3.0\ --csharp_out=.\ProtoGenFiles\r2.3.0\  *.proto
